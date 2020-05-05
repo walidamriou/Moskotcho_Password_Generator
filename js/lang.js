40fabf98205a84f31e14bdb1cd44d0583155c543
Walid Amriou <walidamriou@gmail.com>
2020-05-06 00:01:07 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -14,7 +14,7 @@ function readJsonFile(file, callback) {
 readJsonFile("https://walidamriou.github.io/data.json", function(text){
     var data = JSON.parse(text);
     console.log(data);
-    console.log(data.data1['id'];
+    console.log(data.data1.data1['id'];
     //var elem = document.getElementById("datadisplay");
     //elem.innerHTML = data.data['id']; //we want to read: "id": "123664" 
 });
