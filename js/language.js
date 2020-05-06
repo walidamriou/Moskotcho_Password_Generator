874111d6908a5f6b4ae9d8566c45809941ca61e4
Walid Amriou <walidamriou@gmail.com>
2020-05-06 11:34:25 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,4 +1,3 @@
-/*
 var English = "https://moskotchopg.walidamriou.com/language/en.json";
 var Arabic = "https://moskotchopg.walidamriou.com/language/ar.json";
 
@@ -19,24 +18,12 @@ function readJsonFile(file) {
   });
 }
 
-function ReadLanguage(language) {
+function readAndAppend(language) {
   readJsonFile(language).then(function (data) {
     console.log(data);
     request_data = data;
-    document.getElementById("test").innerHTML = request_data.English['Help_text'];
-
   });
 }
 
-var default_language = ReadLanguage(English);
-//ReadLanguage(Arabic['Help_text']);
-//console.log(request_data['Help_text']);
-function BodyTextstructure(){
-  //change by id
-  //default_language
-  document.getElementById("your_new_password_text").innerHTML = default_language;
-
-
-}
-
-*/
+readAndAppend(English);
+readAndAppend(Arabic);
